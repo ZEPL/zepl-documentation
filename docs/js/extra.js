89f25dc01152d6d7167de0892c6bd5729e130875
AhyoungRyu <fbdkdud93@hanmail.net>
2017-09-18 11:40:55 +0900

    Remove unused file: extra.js

--- docs/js/extra.js
+++
@@ -1,7 +0,0 @@
-//Make page redirect
-var originalUrl = 'http://zepl.github.io/zepl-documentation/';
-var newUrl = 'http://docs.zepl.com/';
-
-if ( window.location == originalUrl) {
-  window.location.href = newUrl;
-}
